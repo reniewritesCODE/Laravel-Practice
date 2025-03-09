7e502c3edfd24c29bf47d7ef23610aaa8aa20e0a
reniewritesCODE <reniewritescode@gmail.com>
2025-03-10 01:55:48 +0800

    modified home page and renamed the blade filename

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('test');
+    return view('welcome');
 });
 
-Route::get('test2', function () {
-    return view('test2');
-});
+// Route::get('test2', function () {
+//     return view('test2');
+// });
+
+Route::view('/home', 'home');
