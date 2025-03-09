7a52c519824dab4dcc37f257f17a2069768ad8d2
reniewritesCODE <reniewritescode@gmail.com>
2025-03-10 02:37:32 +0800

    two more pages: contact and about

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 // Route::get('test2', function () {
 //     return view('test2');
 // });
 
-Route::view('/home', 'home');
+Route::view('/', 'home');
+Route::view('contact', 'contact');
+Route::view('about', 'about');
 // Route::view('/app', 'app');
